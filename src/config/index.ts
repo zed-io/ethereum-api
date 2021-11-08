d1069f63883067ca585c37c6aafbebe7d66970c0
mrkp <mark@zed.io>
2021-11-08 12:17:27 -0400

    changing prod ports

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,7 +9,7 @@ export default {
   env: env,
   debug: debug,
   host: process.env.HOST || `0.0.0.0`,
-  port: process.env.PORT || env === "production" ? 5000 : 5005,
+  port: process.env.PORT || env === "production" ? 3000 : 3001,
   infura: {
     id: process.env.INFURA_PROJECT_ID,
     secret: process.env.INFURA_PROJECT_SECRET,
